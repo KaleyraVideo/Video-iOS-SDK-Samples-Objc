aa1d3e88afb15c35aa21028dc9feb723cc6b427a
Marco Brescianini <m.brescianini85@gmail.com>
2019-02-19 10:30:19 +0100

    Update app delegate comments

--- CallKit-Voip-Example/CallKit-Voip-Example/AppDelegate.m
+++ CallKit-Voip-Example/CallKit-Voip-Example/AppDelegate.m
@@ -4,6 +4,7 @@
 
 #import <PushKit/PushKit.h>
 #import <BandyerSDK/BandyerSDK.h>
+
 #import "AppDelegate.h"
 
 @interface AppDelegate () <PKPushRegistryDelegate, BCXCallClientObserver>
@@ -18,21 +19,59 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    //Before you can get started, you must review your project configuration, and enable the required
-    //app capabilities for CallKit and Voip notifications. Namely, you must enable "Background modes" capability
+    //Before we can get started, you must review your project configuration, and enable the required
+    //app capabilities for CallKit and Voip notifications.
+    //
+    //Namely, you must enable "Background modes" capability
     //checking "Audio, AirPlay and Picture in Picture" and "Voice over IP" checkboxes on.
-    //You must also enable "Push notifications" capability even if you use VOIP notifications only
+    //You must also enable "Push notifications" capability even if you use VOIP notifications only.
+    //
+    //Privacy usage descriptions:
+    //You must add NSCameraUsageDescription and NSMicrophoneUsageDescription to your app Info.plist file.
+    //Those values are required to access microphone and camera. In this example app, those values have been already added for you
+    //
+    //CallKit:
+    //CallKit framework must be linked to your app and it must linked as a required framework,
+    //otherwise the app will have a weird behaviour when it is launched upon receiving a voip notification.
+    //It is going to be launched, but the system is going to suspend it after few milliseconds.
+    //In this example app, the CallKit framework has been already added for you.
 
     BDKEnvironment *env = BDKEnvironment.sandbox;
     BDKConfig *config = [BDKConfig new];
     config.environment = env;
+
+    //On iOS 10 and above this statement is not needed, the default configuration object
+    //enables CallKit by default, it is here for completeness sake
     config.callKitEnabled = YES;
+
+    //The following statement is going to change the name of the app that is going to be shown by the system call UI.
+    //If you don't set this value during the configuration, the SDK will look for to the value of the
+    //CFBundleDisplayName key (or the CFBundleName, if the former is not available) found in your App Info.plist
+
+    //config.nativeUILocalizedName = @"My wonderful app";
+
+    //The following statement is going to change the ringtone used by the system call UI when an incoming call
+    //is received. You should provide the name of the sound resource in the app bundle that is going to be used as
+    //ringtone. If you don't set this value, the SDK will use the default system ringtone.
+
+    //config.nativeUIRingToneSound = @"MyRingtoneSound";
+
+    //The following statements are going to change the app icon shown in the system call UI. When the user answers
+    //a call from the lock screen or when the app is not in foreground and a call is in progress, the system
+    //presents the system call UI to the end user. One of the buttons gives the user the ability to get back into your
+    //app. The following statements allows you to change that icon.
+    //Beware, the configuration object property expects the image as an NSData object. You must provide a side
+    //length 40 points square png image.
+    //It is highly recommended to set this property, otherwise a "question mark" icon placeholder is used instead.
+
+    //UIImage *callKitIconImage = [UIImage imageNamed:@"IMAGE FROM APP BUNDLE OR XCAsset"];
+    //config.nativeUITemplateIconImageData = UIImagePNGRepresentation(callKitIconImage);
 
     [BandyerSDK.instance initializeWithApplicationId:@"YOUR_APP_ID" config:config];
     [BandyerSDK.instance.callClient addObserver:self queue:dispatch_get_main_queue()];
-
+    
 #if !TARGET_IPHONE_SIMULATOR
-    self.registry = [[PKPushRegistry alloc] initWithQueue:nil];
+    self.registry = [[PKPushRegistry alloc] initWithQueue:dispatch_get_main_queue()];
     self.registry.delegate = self;
     self.registry.desiredPushTypes = [NSSet setWithObject:PKPushTypeVoIP];
 #endif
@@ -85,6 +124,7 @@
 - (void)pushRegistry:(PKPushRegistry *)registry didUpdatePushCredentials:(PKPushCredentials *)pushCredentials forType:(PKPushType)type
 {
     //Update push token credential in your back-end system
+    NSLog(@"Updated push credentials %@", pushCredentials.token);
 }
 
 - (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(PKPushType)type
@@ -132,6 +172,8 @@
 - (void)handlePushPayload:(PKPushPayload *)payload
 {
     NSDictionary *dictionaryPayload = payload.dictionaryPayload;
+
+    //You must change the keypath otherwise notifications won't be handled by the sdk
     NSDictionary *incomingCallPayload = [dictionaryPayload valueForKeyPath:@"KEYPATH_TO_DATA_DICTIONARY"];
 
     //We ask the client to handle the notification payload
