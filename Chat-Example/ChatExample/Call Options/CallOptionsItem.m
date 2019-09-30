07f4df4abc85b152436518cbea1ddd419bb6ae4a
luca.tagliabue <luca.tagliabue@bandyer.com>
2019-09-30 18:08:05 +0200

    Init of Chat Example project
    Add implementation for BandyerSDK requirements

---
+++ Chat-Example/ChatExample/Call Options/CallOptionsItem.m
@@ -0,0 +1,38 @@
+//
+//  Copyright © 2019 Bandyer. All rights reserved.
+//
+
+#import "CallOptionsItem.h"
+
+
+@implementation CallOptionsItem
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self)
+    {
+        _type = BDKCallTypeAudioVideo;
+        _record = NO;
+        _maximumDuration = 0;
+    }
+
+    return self;
+}
+
+
+- (id)copyWithZone:(nullable NSZone *)zone
+{
+    CallOptionsItem *copy = (CallOptionsItem *) [[[self class] allocWithZone:zone] init];
+
+    if (copy != nil)
+    {
+        copy.type = self.type;
+        copy.record = self.record;
+        copy.maximumDuration = self.maximumDuration;
+    }
+
+    return copy;
+}
+
+@end
---
+++ Chat-Example/ChatExample/Services/UserInfoFetcher.h
@@ -0,0 +1,23 @@
+//
+//  Copyright © 2019 Bandyer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <BandyerSDK/BandyerSDK.h>
+
+@class AddressBook;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UserInfoFetcher : NSObject <BDKUserInfoFetcher>
+
+@property (nonatomic, strong, readonly) AddressBook *addressBook;
+
+- (instancetype)initWithAddressBook:(AddressBook *)addressBook;
+
+- (instancetype)init NS_UNAVAILABLE;
++ (instancetype)new NS_UNAVAILABLE;
+
+@end
+
+NS_ASSUME_NONNULL_END
