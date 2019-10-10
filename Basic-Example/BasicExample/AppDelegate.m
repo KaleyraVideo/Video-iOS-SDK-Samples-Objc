c9b2558ce97787527027c5087206bbe4f995efea
luca.tagliabue <luca.tagliabue@bandyer.com>
2019-10-10 10:54:02 +0200

    Add the NSCameraUsageDescription and NSMicrophoneUsageDescription keys to Info.plist.
    Edit the comment inside AppDelegate accordingly.

--- Basic-Example/BasicExample/AppDelegate.m
+++ Basic-Example/BasicExample/AppDelegate.m
@@ -14,10 +14,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    //Before we dive into the details of how the SDK must be configured and initialized
+    //Before we dive into the details of how the SDK must be configured and initialized,
     //you should add NSCameraUsageDescription and NSMicrophoneUsageDescription keys into app Info.plist
     //if you haven't done so already, otherwise your app is going to crash anytime it tries to access camera
-    //or microphone devices.
+    //or microphone devices. In this example app, those values have been already added for you.
     
     //Here we are going to initialize the Bandyer SDK.
     //The sdk needs a configuration object where it is specified which environment the sdk should work in.
