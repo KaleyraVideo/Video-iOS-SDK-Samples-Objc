90298a807ef9c70391d87e5d7f95e255c1ed9e1a
Marco Brescianini <marco.brescianini@bandyer.com>
2021-02-16 10:59:56 +0100

    Fix broken import in UI customisation example

--- UI-Customization-Example/UI-Customization-Example/Services/UserDetailsProvider.m
+++ UI-Customization-Example/UI-Customization-Example/Services/UserDetailsProvider.m
@@ -2,7 +2,7 @@
 //  Copyright © 2020 Bandyer. All rights reserved.
 //  See LICENSE.txt for licensing information
 
-#import "UserInfoFetcher.h"
+#import "UserDetailsProvider.h"
 #import "Contact.h"
 #import "AddressBook.h"
 
