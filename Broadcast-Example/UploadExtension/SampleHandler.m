c84d148265a4b80b8b46745779cef040b250a0da
Marco Brescianini <marco.brescianini@bandyer.com>
2021-06-28 15:20:53 +0200

    Update UploadExtension SampleHandler with calls to BroadcastExtensionFramework

--- Broadcast-Example/UploadExtension/SampleHandler.m
+++ Broadcast-Example/UploadExtension/SampleHandler.m
@@ -4,40 +4,37 @@
 
 #import "SampleHandler.h"
 
+#import <BandyerBroadcastExtension/BandyerBroadcastExtension.h>
+
 @implementation SampleHandler
 
-- (void)broadcastStartedWithSetupInfo:(NSDictionary<NSString *,NSObject *> *)setupInfo {
-    // User has requested to start the broadcast. Setup info from the UI extension can be supplied but optional. 
+- (void)broadcastStartedWithSetupInfo:(NSDictionary<NSString *,NSObject *> *)setupInfo
+{
+    __weak __typeof__(self) _wself = self;
+    [BBEBroadcastExtension.instance startWithAppGroupIdentifier:@"group.com.acme.BroadcastExample" setupInfo:setupInfo errorHandler:^(NSError * _Nonnull error) {
+        __strong __typeof__(_wself) sself = _wself;
+        [sself finishBroadcastWithError:error];
+    }];
 }
 
-- (void)broadcastPaused {
-    // User has requested to pause the broadcast. Samples will stop being delivered.
+- (void)broadcastPaused
+{
+    [BBEBroadcastExtension.instance pause];
 }
 
-- (void)broadcastResumed {
-    // User has requested to resume the broadcast. Samples delivery will resume.
+- (void)broadcastResumed
+{
+    [BBEBroadcastExtension.instance resume];
 }
 
-- (void)broadcastFinished {
-    // User has requested to finish the broadcast.
+- (void)broadcastFinished
+{
+    [BBEBroadcastExtension.instance finish];
 }
 
-- (void)processSampleBuffer:(CMSampleBufferRef)sampleBuffer withType:(RPSampleBufferType)sampleBufferType {
-    
-    switch (sampleBufferType) {
-        case RPSampleBufferTypeVideo:
-            // Handle video sample buffer
-            break;
-        case RPSampleBufferTypeAudioApp:
-            // Handle audio sample buffer for app audio
-            break;
-        case RPSampleBufferTypeAudioMic:
-            // Handle audio sample buffer for mic audio
-            break;
-            
-        default:
-            break;
-    }
+- (void)processSampleBuffer:(CMSampleBufferRef)sampleBuffer withType:(RPSampleBufferType)sampleBufferType
+{
+    [BBEBroadcastExtension.instance processSampleBuffer:sampleBuffer ofType:sampleBufferType];
 }
 
 @end
