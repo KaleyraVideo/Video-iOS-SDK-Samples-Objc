6f645e6d5a3a5dabade673db428a5ce37c14a704
Marco Brescianini <marco.brescianini@bandyer.com>
2021-06-28 15:40:08 +0200

    Update SampleHandler replacing group identifier with a placeholder value

--- Broadcast-Example/UploadExtension/SampleHandler.m
+++ Broadcast-Example/UploadExtension/SampleHandler.m
@@ -10,8 +10,9 @@
 
 - (void)broadcastStartedWithSetupInfo:(NSDictionary<NSString *,NSObject *> *)setupInfo
 {
+#error "Replace APP_GROUP_IDENTIFIER_GOES_HERE placeholder with your app group identifier"
     __weak __typeof__(self) _wself = self;
-    [BBEBroadcastExtension.instance startWithAppGroupIdentifier:@"group.com.acme.BroadcastExample" setupInfo:setupInfo errorHandler:^(NSError * _Nonnull error) {
+    [BBEBroadcastExtension.instance startWithAppGroupIdentifier:@"APP_GROUP_IDENTIFIER_GOES_HERE" setupInfo:setupInfo errorHandler:^(NSError * _Nonnull error) {
         __strong __typeof__(_wself) sself = _wself;
         [sself finishBroadcastWithError:error];
     }];
